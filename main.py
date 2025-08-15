5bdd6822b6104bdacea0e33c03a95ffa812eb218
cherrypy8 <codefury8.0@gmail.com>
2025-08-16 02:32:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -74,6 +74,8 @@ Now give two bullet points for the given weather:
 
     except Exception as e:
         return {"error": str(e)}
+
+
 # ---------------- Gemini Chat ----------------
 
 
@@ -182,4 +184,3 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
     import uvicorn
     uvicorn.run("main:app", host="0.0.0.0", port=port)
-
